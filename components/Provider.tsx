2f12694d2aa96a6fe485cbc65ec9bc0ab6b17e61
Sid <starindustries05@gmail.com>
2023-06-18 19:31:24 +0530

    changes

--- components/Provider.tsx
+++ components/Provider.tsx
@@ -2,5 +2,5 @@
 import { SessionProvider } from "next-auth/react";
 
 export default function Provider({ children }: { children: React.ReactNode }) {
-	return <SessionProvider session={null}>{children}</SessionProvider>;
+	return <SessionProvider>{children}</SessionProvider>;
 }
